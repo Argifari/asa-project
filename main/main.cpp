1c40cded14c185ecb59d7a0c4ea4cb7e80639232
Argifari <argifari40@gmail.com>
2026-06-07 08:35:06 +0700

    Commit 3.0

--- main/main.cpp
+++ main/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <chrono>
 #include "./algoritma/json.hpp"
 #include "./variables/variables.h"
 #include "./algoritma/greedy/greedy.h"
@@ -13,6 +14,18 @@
 
 using namespace std;
 
+#include <vector>
+
+void flushCache() {
+    const size_t cacheSize = 32 * 1024 * 1024; // 32 Megabytes
+    std::vector<char> dummy(cacheSize, 1);
+
+    volatile char sink = 0;
+    for (size_t i = 0; i < cacheSize; i += 64) { // Loncat 64 byte (ukuran standar baris cache / cache line)
+        sink = dummy[i];
+    }
+}
+
 vector <MoveData> databaseSol;
 
 
@@ -21,16 +34,58 @@ int main () {
     cin.tie(0);
 
     readMoveData("../data/solData.json", databaseSol);
+    int starterId = 1;
 
-    greedyAlgorithm(1,databaseSol);
+    cout<<"=== MEMULAI PENGUJIAN PERFORMA ALGORITMA ===\n\n";
 
-    BFSAlgorithm(1,databaseSol);
+    // PENGUJIAN GREEDY
+    auto startGreedy = chrono::high_resolution_clock::now();
+    greedyAlgorithm(starterId,databaseSol);
+    auto endGreedy = chrono::high_resolution_clock::now();
 
-    DFSAlgorithm(1,databaseSol);
+    auto durationGreedy = chrono::duration_cast<chrono::microseconds>(endGreedy - startGreedy).count();
+    cout << "Waktu Eksekusi GREEDY : " << durationGreedy << " mikrodetik\n\n";
 
-    DPAlgorithm(1,databaseSol);
+    flushCache();
 
-    GeneticAlgorithm(1,databaseSol);
+    // PENGUJIAN DFS
+    auto startDFS = chrono::high_resolution_clock::now();
+    DFSAlgorithm(starterId,databaseSol);
+    auto endDFS = chrono::high_resolution_clock::now();
+    
+    auto durationDFS = chrono::duration_cast<chrono::microseconds>(endDFS - startDFS).count();
+    cout << "Waktu Eksekusi DFS : " << durationDFS << " mikrodetik\n\n";
 
+    flushCache();
+
+    // PENGUJIAN BFS
+    auto startBFS = chrono::high_resolution_clock::now();
+    BFSAlgorithm(starterId,databaseSol);
+    auto endBFS = chrono::high_resolution_clock::now();
+    
+    auto durationBFS = chrono::duration_cast<chrono::microseconds>(endBFS - startBFS).count();
+    cout << "Waktu Eksekusi BFS : " << durationBFS << " mikrodetik\n\n";
+
+    flushCache();
+
+
+    // PENGUJIAN DP
+    auto startDP = chrono::high_resolution_clock::now();
+    DPAlgorithm(starterId,databaseSol);
+    auto endDP = chrono::high_resolution_clock::now();
+    
+    auto durationDP = chrono::duration_cast<chrono::microseconds>(endDP - startDP).count();
+    cout << "Waktu Eksekusi DP : " << durationDP << " mikrodetik\n\n";
+    
+    
+    flushCache();
+    
+    
+    auto startGA = chrono::high_resolution_clock::now();
+    GeneticAlgorithm(starterId,databaseSol);
+    auto endGA = chrono::high_resolution_clock::now();
+    
+    auto durationGA = chrono::duration_cast<chrono::microseconds>(endGA - startGA).count();
+    cout << "Waktu Eksekusi GA : " << durationGA << " mikrodetik\n\n";
     return 0;
 }
